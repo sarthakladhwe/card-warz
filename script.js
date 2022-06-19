5ebe6fa89c6d972d08c415ad49941425964237b0
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-19 17:30:29 +0530

    Fetching deck and drawing cards

---
+++ script.js
@@ -0,0 +1,20 @@
+let deckId = ""
+
+function handleClick() {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id;
+            console.log(deckId)
+        })
+}
+
+function drawCards() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById('draw-cards').addEventListener("click", drawCards)
