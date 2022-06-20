5f5cbea72db1c16b33c7b2064f0f53888b1702f5
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-20 23:18:32 +0530

    Reset game functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+drawCardBtn.disabled = true
 const header = document.getElementById("header")
 const remainingCards = document.getElementById("remaining-cards")
 const computerScoreEl = document.getElementById("computer-score")
@@ -20,11 +21,20 @@ function handleClick() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            deckId = data.deck_id;
+            deckId = data.deck_id
+            drawCardBtn.disabled = false
+            cardsContainer.children[0].innerHTML = ""
+            cardsContainer.children[1].innerHTML = ""
+            header.textContent = "Game of War"
+            remainingCards.textContent = ""
+            computerScore = 0
+            myScore = 0
+            computerScoreEl.textContent = `Computer score: ${computerScore}`
+            myScoreEl.textContent = `My score: ${myScore}`
         })
 }
 
-function drawCards() {
+function drawCards() { 
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
@@ -45,8 +55,6 @@ function drawCards() {
 
 function winningCard(card1, card2) {
 
-    console.log("card1.value", Object.hasOwnProperty(card1.value))
-    
     const card1Value = cardValues.hasOwnProperty(card1.value) ? cardValues[card1.value] : parseInt(card1.value)
     const card2Value = cardValues.hasOwnProperty(card2.value) ? cardValues[card2.value] : parseInt(card2.value)
 
